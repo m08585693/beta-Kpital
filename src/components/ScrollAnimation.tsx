12724909ec2435303b529475ed2341ba419a773a
m08585693 <m08585693@gmail.com>
2026-05-11 17:36:28 +0000

    add ScrollAnimation component

--- src/components/ScrollAnimation.tsx
+++ src/components/ScrollAnimation.tsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import { useRef, useState, useEffect } from 'react';
 
 interface Props {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   className?: string;
 }
 
